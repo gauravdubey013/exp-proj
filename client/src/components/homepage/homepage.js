4e96d7a87ac9e3a50f189419807796f4ae0e4d40
gauravdubey013 <dubeygaurav520@gmail.com>
2023-12-11 21:38:15 +0530

    react-log-in-out - mern stack

--- client/src/components/homepage/homepage.js
+++ client/src/components/homepage/homepage.js
@@ -1,13 +1,15 @@
-import React from "react"
-import "./homepage.css"
+import React from "react";
+import "./homepage.css";
 
-const Homepage = ({setLoginUser}) => {
-    return (
-        <div className="homepage">
-            <h1>Hello Homepage</h1>
-            <div className="button" onClick={() => setLoginUser({})} >Logout</div>
-        </div>
-    )
-}
+const Homepage = ({ setLoginUser }) => {
+  return (
+    <div className="homepage flex items-center justify-center">
+      <h1>Hello Homepage</h1>
+      <div className="button" onClick={() => setLoginUser({})}>
+        Logout
+      </div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
